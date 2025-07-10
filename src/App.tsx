265e04006e8ead5c766549f4b079449b19f40212
Дмитрий Кудюкин <dimadk@MacBookPro.lan>
2025-07-10 14:01:08 +0300

    add admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
+import AdminPanel from './pages/AdminPanel';
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      {/* Главная страница */}
+      <Route path="/" element={<HomePage />} />
+      
+      {/* Админ-панель */}
+      <Route path="/admin/*" element={<AdminPanel />} />
+    </Routes>
+  );
 }
 
 export default App;

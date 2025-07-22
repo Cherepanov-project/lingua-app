ebcf1ca3e5c93a069cb15b7b08d7e59fd14ffa3c
Anya Pham <kieuanh997@gmail.com>
2025-07-22 22:49:59 +0300

    .

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { authApi } from '../user/features/auth/authApi';
+import { configureStore } from "@reduxjs/toolkit";
+import { authApi } from "../user/features/auth/authApi";
+import { languagesApi } from "../shared/api/languagesApi";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [languagesApi.reducerPath]: languagesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, languagesApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

64cb7d884691781129598309f7429f343945bace
SD <jake2017tg@gmail.com>
2025-07-31 17:05:26 +0300

    fix(redux): add languages API middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from '../user/features/auth/authApi';
+import { languagesApi } from '../shared/api/languagesApi';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [languagesApi.reducerPath]: languagesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(languagesApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;

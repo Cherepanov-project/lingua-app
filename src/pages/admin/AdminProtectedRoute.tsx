83bf24a0ff74f81c20db5488a82a89b123928e7d
Artem <skreeetie@gmail.com>
2025-09-21 18:43:59 +0300

    Реализована роль админа для доступа к админ панели

---
+++ src/pages/admin/AdminProtectedRoute.tsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useGetUserRolesQuery } from "../../shared/api/usersApi";
+import { getCookie } from "../../user/utils/cookies";
+import { jwtDecode } from "jwt-decode";
+
+interface AdminProtectedProps {
+  children: React.ReactNode;
+}
+
+interface JwtPayload {
+  sub: string;
+}
+
+const AdminProtectedRoute = ({ children }: AdminProtectedProps) => {
+  const token = getCookie("auth_token");
+  const navigate = useNavigate();
+
+  let userId: string | undefined;
+  let decodedToken: JwtPayload | undefined;
+  if (token) {
+    try {
+      decodedToken = jwtDecode<JwtPayload>(token);
+      userId = decodedToken.sub;
+    } catch (e) {
+      console.error("Ошибка декодирования токена:", e);
+    }
+  }
+  const { data: userRoles, isLoading } = useGetUserRolesQuery(
+    userId as string,
+    { skip: !token || !userId }
+  );
+  if (!isLoading) {
+    if (userRoles?.map((role) => role.name).includes("admin")) {
+      return <>{children}</>;
+    } else {
+      setTimeout(() => {
+        navigate("/profile");
+      }, 1500);
+      return (
+        <h2
+          style={{ color: "red", margin: "24px auto", maxWidth: "fit-content" }}
+        >
+          Нет доступа.
+        </h2>
+      );
+    }
+  }
+};
+
+export default AdminProtectedRoute;

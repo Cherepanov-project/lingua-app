4bfbacd7d74152c0429d69ecdbb9b5fa6d21308d
Andrew <tonkachiov@gmail.com>
2025-09-21 19:53:38 +0300

    fix: ui profilepage

--- src/user/components/Profile/ProfileWidgetGames.tsx
+++ src/user/components/Profile/ProfileWidgetGames.tsx
@@ -1,18 +1,20 @@
-import { Stack } from '@mui/material'
-import { Typography } from '@mui/material'
-import { ProfileWidgetSlider } from './ProfileWidgetSlider'
-import { mockDataGamesSlider } from './mockDataSlider'
+import { Stack } from "@mui/material";
+import { Typography } from "@mui/material";
+import { ProfileWidgetSlider } from "./ProfileWidgetSlider";
+import { mockDataGamesSlider } from "./mockDataSlider";
 
 const ProfileWidgetGames = () => {
   return (
     <Stack>
-      <Stack className="car" sx={{ minHeight: '100px' }}>
+      <Stack className="car" sx={{ minHeight: "90px" }}>
         <Typography variant="h4">Игры</Typography>
-        <Typography sx={{ color: '#878787', fontSize: '20px' }}>Играй и забирай звезды</Typography>
+        <Typography sx={{ color: "#878787", fontSize: "20px" }}>
+          Играй и забирай звезды
+        </Typography>
       </Stack>
       <ProfileWidgetSlider itemList={mockDataGamesSlider} />
     </Stack>
-  )
-}
+  );
+};
 
-export { ProfileWidgetGames }
+export { ProfileWidgetGames };
--- src/user/components/Profile/ProfileWidgetGrammar.tsx
+++ src/user/components/Profile/ProfileWidgetGrammar.tsx
@@ -1,17 +1,17 @@
-import { Typography } from '@mui/material'
-import { Stack } from '@mui/material'
-import { ProfileWidgetSlider } from './ProfileWidgetSlider'
-import { mockDataGrammarSlider } from './mockDataSlider'
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/material";
+import { ProfileWidgetSlider } from "./ProfileWidgetSlider";
+import { mockDataGrammarSlider } from "./mockDataSlider";
 
 const ProfileWidgetGrammar = () => {
   return (
     <Stack>
-      <Stack sx={{ minHeight: '100px' }}>
+      <Stack sx={{ minHeight: "90px" }}>
         <Typography variant="h4">Грамматика</Typography>
       </Stack>
       <ProfileWidgetSlider itemList={mockDataGrammarSlider} />
     </Stack>
-  )
-}
+  );
+};
 
-export { ProfileWidgetGrammar }
+export { ProfileWidgetGrammar };

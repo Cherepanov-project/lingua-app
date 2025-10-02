c283bae769c9095e74942d2edeb6469c8b8aa948
skreeetie <sqdteffy@gmail.com>
2025-10-02 22:40:11 +0700

    Revert "Revert "Feature/access control" (#28)" (#31)
    This reverts commit d17594b30d2254bcf6904dde2b59f3c490517cbc.

--- src/shared/api/picturesApi.ts
+++ src/shared/api/picturesApi.ts
@@ -1,46 +1,48 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 type Picture = {
-  id: number
-  img: string
-  title: string
-  tag: string
-}
+  id: number;
+  img: string;
+  title: string;
+  tag: string;
+};
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
 export const picturesApi = createApi({
-  reducerPath: 'userPictures',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
-  tagTypes: ['Pictures'],
+  reducerPath: "userPictures",
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  tagTypes: ["Pictures"],
   endpoints: (builder) => ({
     getPictures: builder.query<Picture[], void>({
-      query: () => 'pictures',
+      query: () => "pictures",
       providesTags: (result) =>
         result
           ? [
-              ...result.map(({ id }) => ({ type: 'Pictures' as const, id })),
-              { type: 'Pictures', id: 'LIST' },
+              ...result.map(({ id }) => ({ type: "Pictures" as const, id })),
+              { type: "Pictures", id: "LIST" },
             ]
-          : [{ type: 'Pictures', id: 'LIST' }],
+          : [{ type: "Pictures", id: "LIST" }],
     }),
-    
+
     getPicture: builder.query<Picture, number>({
       query: (id) => `pictures/${id}`,
-      providesTags: (_result, _error, id) => [{ type: 'Pictures', id }],
+      providesTags: (_result, _error, id) => [{ type: "Pictures", id }],
     }),
-    
-    addPicture: builder.mutation<Picture, Omit<Picture, 'id'>>({
+
+    addPicture: builder.mutation<Picture, Omit<Picture, "id">>({
       query: (newPicture) => ({
-        url: 'pictures',
-        method: 'POST',
+        url: "pictures",
+        method: "POST",
         body: newPicture,
       }),
-      invalidatesTags: [{ type: 'Pictures', id: 'LIST' }], 
+      invalidatesTags: [{ type: "Pictures", id: "LIST" }],
     }),
   }),
 });
 
-
-export const { 
-  useGetPicturesQuery, 
+export const {
+  useGetPicturesQuery,
   useGetPictureQuery,
   useAddPictureMutation,
 } = picturesApi;

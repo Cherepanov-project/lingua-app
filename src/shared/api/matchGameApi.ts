9f924ff1c29715a2bcf920e66865aa83869fac04
skreeetie <sqdteffy@gmail.com>
2025-10-10 00:58:26 +0700

    Feature/find pair (#35)
    * Работа над вкладкой с добавлением игр в админ-панели, реализованы базовые стили и модалка
    
    * работа над конструктором
    
    * добавление новых данных в моковую базу, реализовано добавление новых пар через модалку
    
    * реализовано удаление уровней
    
    * реализовано редактирование существующего уровня, мелкие исправление
    
    * ...
    
    * fixes
    
    * ...
    
    ---------
    
    Co-authored-by: Artem <skreeetie@gmail.com>

--- src/shared/api/matchGameApi.ts
+++ src/shared/api/matchGameApi.ts
@@ -9,7 +9,8 @@ export type MatchGame = {
   }[];
 };
 
-const API_BASE_URL = import.meta.env.BASE_URL || "http://localhost:3001";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
 export const matchGamesApi = createApi({
   reducerPath: "matchGames",

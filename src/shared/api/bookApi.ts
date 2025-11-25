4b68aa0f198f522a0b06d4c3ec6046dc87cf51ad
Aleksandr Volkov <aev7823@gmail.com>
2025-11-25 22:51:57 +0700

    Пофиксил конфликт vercel

--- src/shared/api/bookApi.ts
+++ src/shared/api/bookApi.ts
@@ -2,22 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8787'
 
-// export const bookApi = createApi({
-//   reducerPath: "bookApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "/", 
-//   }),
-//   endpoints: (builder) => ({
-//     getBookHtml: builder.query<string, string>({
-//       query: (url) => ({
-//         url,         
-//         method: "GET",
-//         responseHandler: (response) => response.text(), 
-//       }),
-//     }),
-//   }),
-// });
-
 export const bookApi = createApi({
   reducerPath: 'bookApi',
   baseQuery: fetchBaseQuery({ baseUrl: `${API_BASE_URL}/api/` }),
